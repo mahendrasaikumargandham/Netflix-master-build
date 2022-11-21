14e0953fe9d917984b069e00d2470056d7e83cae
mahendrasaikumargandham <mahendragandham730@gmail.com>
2022-11-21 11:35:07 +0530

    Added build

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLMJNKa5mvlOozgWoQCjeCmDqLihKz-lQ",
-  authDomain: "netflix-master-build.firebaseapp.com",
-  projectId: "netflix-master-build",
-  storageBucket: "netflix-master-build.appspot.com",
-  messagingSenderId: "100892276256",
-  appId: "1:100892276256:web:0d4fff9fc6f6defeb32e45",
-  measurementId: "G-WX3V3L2K0D",
+  apiKey: "AIzaSyDnxWISey97Yx3DpWoPL9p05WD2fSdye_k",
+  authDomain: "netflix-build-ea9ad.firebaseapp.com",
+  projectId: "netflix-build-ea9ad",
+  storageBucket: "netflix-build-ea9ad.appspot.com",
+  messagingSenderId: "697537803003",
+  appId: "1:697537803003:web:836540ce2bbe761768765e",
 };
 
 const app = initializeApp(firebaseConfig);
